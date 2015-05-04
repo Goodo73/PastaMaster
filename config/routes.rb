2f8a35251b3983bf71845bc981aa04cde00f2d98
Louis Moore <burntliketoast@gmail.com>
2015-05-04 15:51:01 +1000

    Started user auth. Updated gem dependencies for bourbon + neat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
 
+
   resources :recipes
+  resources :users
+
   get '/' => 'recipes#index', :as => :root
-  
+
+
+  # ------ Authentication routes ------
+  # /session/new     login form
+  get '/login' => 'session#new'
+  # /session         logout
+  delete '/logout' => 'session#destroy'
+  # /session         after submit form for login
+  post '/login' => 'session#create'
+  # ----------------------------------
+
 end
