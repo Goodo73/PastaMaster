96440a8840b9acac3a4c78743c0f10e6e6fe8cd6
Louis Moore <burntliketoast@gmail.com>
2015-05-06 10:05:33 +1000

    Modal logic added (some random html markup crap to test in application.html.erb)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,3 +36,25 @@ $(document).ready(function() {
 });
 //  ---------------------------------
 
+
+
+// ------ Modal animation ------
+
+$(function() {
+  $("#modal-1").on("change", function() {
+    if ($(this).is(":checked")) {
+      $("body").addClass("modal-open");
+    } else {
+      $("body").removeClass("modal-open");
+    }
+  });
+
+  $(".modal-fade-screen, .modal-close").on("click", function() {
+    $(".modal-state:checked").prop("checked", false).change();
+  });
+
+  $(".modal-inner").on("click", function(e) {
+    e.stopPropagation();
+  });
+});
+//  ---------------------------------
